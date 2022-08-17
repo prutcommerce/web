4f13a1559985b9d450e4572b68f9d8cbc84a5c86
miutamihai <miuta.mihai@gmail.com>
2022-08-17 03:16:03 +0300

    [master] Add initial graphql setup

--- src/App/Products/Initial.elm
+++ src/App/Products/Initial.elm
@@ -1,9 +1,61 @@
-module App.Products.Initial exposing (initial)
+module App.Products.Initial exposing (query, ProductConnection, ProductEdge)
 
-import App.Products.Type exposing (Product)
+import App.Products.Type
+import Artemis.Query as Query exposing (ProductsOptionalArguments)
+import Graphql.Operation exposing (RootQuery)
+import Graphql.OptionalArgument exposing (OptionalArgument(..))
+import Graphql.SelectionSet as SelectionSet exposing (SelectionSet, with)
+import Artemis.Scalar exposing (..)
+import Artemis.Object
+import Artemis.Object.ProductEdge as Edge
+import Artemis.Object.ProductConnection as Connection
+import Artemis.Object.Product as Product
+
+type alias ProductNode =
+    { id : Id
+    , name : String
+    }
+
+type alias ProductEdge =
+    { node: ProductNode
+    }
+
+type alias ProductConnection =
+    { edges: List (ProductEdge)
+    }
+
+images: List String
+images =
+    [ "https://cdn.britannica.com/65/132165-050-EF2D11F2/roller-skater.jpg"
+    , "https://global-uploads.webflow.com/5ca5fe687e34be0992df1fbe/62841e551612bd1a927b44f3_boy-on-roller-skating-class-2021-08-26-16-53-45-utc-min.jpg"
+    , "https://pyxis.nymag.com/v1/imgs/a2e/fbe/d0b26f8723437d87059c91f1fd965b5a32-bic-roller-skates-final.2x.rsocial.w600.jpg"
+    ]
+
+nodeSelection: SelectionSet ProductNode Artemis.Object.Product
+nodeSelection =
+    SelectionSet.succeed ProductNode
+        |> with Product.id
+        |> with Product.name
+
+edgeSelection: SelectionSet ProductEdge Artemis.Object.ProductEdge
+edgeSelection =
+    SelectionSet.succeed ProductEdge
+        |> with (Edge.node nodeSelection)
+
+connectionSelection: SelectionSet ProductConnection Artemis.Object.ProductConnection
+connectionSelection =
+    SelectionSet.succeed ProductConnection
+        |> with (Connection.edges edgeSelection)
+
+arguments: ProductsOptionalArguments -> ProductsOptionalArguments
+arguments _ = { first = Present 10, after = Absent }
+
+query: SelectionSet ProductConnection RootQuery
+query =
+    Query.products arguments connectionSelection
 
 
-initial : List Product
+initial : List App.Products.Type.Product
 initial =
     [ { id = "id1", name = "skates1", url = "https://cdn.britannica.com/65/132165-050-EF2D11F2/roller-skater.jpg" }
     , { id = "id2", name = "skates2", url = "https://global-uploads.webflow.com/5ca5fe687e34be0992df1fbe/62841e551612bd1a927b44f3_boy-on-roller-skating-class-2021-08-26-16-53-45-utc-min.jpg" }
--- src/Pages/Products.elm
+++ src/Pages/Products.elm
@@ -1,11 +1,15 @@
-module Pages.Products exposing (Model, Msg, page)
+module Pages.Products exposing (Model, Msg(..), page)
 
 import App.Layout exposing (layout)
 import App.Products exposing (body)
-import App.Products.Initial exposing (initial)
+import App.Products.Initial exposing (ProductConnection, ProductEdge, query)
+import App.Products.Initial exposing (ProductConnection, ProductEdge, query)
 import App.Products.Type exposing (Product)
 import Gen.Params.Products exposing (Params)
+import Graphql.Http
+import Html exposing (Html, text)
 import Page
+import RemoteData exposing (RemoteData(..))
 import Request
 import Shared
 import View exposing (View)
@@ -25,14 +29,22 @@ page shared req =
 -- INIT
 
 
-type alias Model =
-    { products : List Product
-    }
+type alias Response =
+    { products: Maybe ProductConnection }
 
+
+type alias Model =
+   RemoteData (Graphql.Http.Error ProductConnection) ProductConnection
+
+pula: Cmd Msg
+pula =
+       query
+        |> Graphql.Http.queryRequest "http://173.249.46.33:5000/graph"
+        |> Graphql.Http.send (RemoteData.fromResult >> GotProducts)
 
 init : ( Model, Cmd Msg )
 init =
-    ( { products = initial }, Cmd.none )
+    ( RemoteData.Loading, pula )
 
 
 
@@ -41,6 +53,7 @@ init =
 
 type Msg
     = ReplaceMe
+    | GotProducts Model
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -48,6 +61,12 @@ update msg model =
     case msg of
         ReplaceMe ->
             ( model, Cmd.none )
+
+        GotProducts products ->
+            (products, Cmd.none)
+
+
+
 
 
 
@@ -62,9 +81,33 @@ subscriptions model =
 
 -- VIEW
 
+someShit: Model -> List(Html Msg)
+someShit model =
+    case model of
+        NotAsked ->
+            [text "Not executed"]
+
+
+        Loading ->
+            [text "Loading"]
+
+
+        Failure error ->
+            [text "Am crapat sa moara mama"]
+
+
+        Success response ->
+            response.edges |> mapPeMata |> body |> layout
+
+mapPeMata: List(ProductEdge) -> List(Product)
+mapPeMata edges =
+       edges
+        |> List.map(\edge -> {id = "", name = edge.node.name, url = ""})
+
+
 
 view : Model -> View Msg
 view model =
     { title = "Products"
-    , body = body model.products |> layout
+    , body = someShit model
     }
